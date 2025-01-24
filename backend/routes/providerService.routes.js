497a86bcb1ce4ddbb4f4913f53f7428916aaa6cd
Henry <henryasante336@gmail.com>
2025-01-24 15:36:58 +0000

    Add endpoint to retrieve all provider services by provider ID

--- backend/routes/providerService.routes.js
+++ backend/routes/providerService.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createNewProviderService, deleteProviderService, getAllProviderService, getProviderProfile, getSingleProviderService, updateProviderService } from "../controllers/providerService.controller.js"
+import { createNewProviderService, deleteProviderService, getAllProviderService, getAllProviderServicesByProviderId, getProviderProfile, getSingleProviderService, updateProviderService } from "../controllers/providerService.controller.js"
 import { requireAuth, restrict } from "../middlewares/auth.middleware.js"
 import upload from "../config/upload.config.js"
 
@@ -8,6 +8,7 @@ const providerServiceRouter = Router()
 //Provider Service
 providerServiceRouter.get("/", getAllProviderService)
 providerServiceRouter.get("/:id", getSingleProviderService) //Get a single provider service
+providerServiceRouter.get("/provider/:providerId", getAllProviderServicesByProviderId) //Get a single provider service
 providerServiceRouter.post("/", requireAuth, upload.fields([{ name: 'providerServiceImage', maxCount: 10 }]), createNewProviderService)
 providerServiceRouter.patch("/:id", requireAuth, upload.fields([{ name: 'providerServiceImage', maxCount: 10 }]), updateProviderService)
 providerServiceRouter.delete("/:id", requireAuth, deleteProviderService)
